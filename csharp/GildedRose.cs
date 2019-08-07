997fc3e881d634dae8a906050f91605b3aa489e2
agne-ja <47142649+agne-ja@users.noreply.github.com>
2019-08-07 23:52:41 +0300

    Added comments

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -9,11 +9,12 @@ namespace csharp
         {
             this.Items = Items;
         }
-
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                // Check if this item's quality reduces with time (normal item or conjured item)
+                // If quality can go lower (hasn't reached 0 yet), it is reduced by 1
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
@@ -24,12 +25,17 @@ namespace csharp
                         }
                     }
                 }
+                // Check if the item's quality increases with time (aged brie or backstage pass)
+                // If quality can go higher (hasn't reached 50 yet), it is increased by 1
                 else
                 {
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
+                        // Backstage pass quality goes up faster as concert approaches
+                        // If 10 days or less are left and quality can go higher, it is increased by 1 (adding up to +2).
+                        // If 5 days or less are left and quality can go higher, it is increased by 1 again (adding up to +3).
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].SellIn < 11)
@@ -50,14 +56,19 @@ namespace csharp
                         }
                     }
                 }
-
+                // SellIn of any item, except Sulfuras, is reduced.
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
+                // If item has reached the sell by date, the quality:
+                // - reduces by double for normal and conjured items,
+                // - increases by double for aged brie,
+                // - goes to 0 for backstage passes.
                 if (Items[i].SellIn < 0)
                 {
+                    // If quality can go lower for normal and conjured items, it goes down by 1 (adding up to -2).
                     if (Items[i].Name != "Aged Brie")
                     {
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
@@ -70,11 +81,13 @@ namespace csharp
                                 }
                             }
                         }
+                        // If a backstage pass has expired, its quality goes to 0.
                         else
                         {
                             Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
                     }
+                    // If aged brie quality can go higher, it is increased by 1 (adding up to +2).
                     else
                     {
                         if (Items[i].Quality < 50)
