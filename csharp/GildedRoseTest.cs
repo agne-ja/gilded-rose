392392df39eb77d08662da2ee26534119d363647
agne-ja <47142649+agne-ja@users.noreply.github.com>
2019-08-08 15:02:06 +0300

    Added tests for Conjured items

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -84,7 +84,7 @@ namespace csharp
         public void UpdateQuality_AgedBrieSellInDays_Lowers()
         {
             var startingSellIn = 1;
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = startingSellIn, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BRIE, SellIn = startingSellIn, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -96,7 +96,7 @@ namespace csharp
         public void UpdateQuality_AgedBrieQualityBeforeSellIn_IncreasesByOne()
         {
             var startingQuality = 1;
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BRIE, SellIn = 5, Quality = startingQuality } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -108,7 +108,7 @@ namespace csharp
         public void UpdateQuality_AgedBrieQualityAfterSellIn_IncreasesByTwo()
         {
             var startingQuality = 1;
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BRIE, SellIn = 0, Quality = startingQuality } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -119,7 +119,7 @@ namespace csharp
         [Test]
         public void UpdateQuality_AgedBrieQualityUpperLimitExceededBeforeSellIn_QualityStaysAtLimit()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = QualityUpperLimit } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BRIE, SellIn = 5, Quality = QualityUpperLimit } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -130,8 +130,8 @@ namespace csharp
         [Test]
         public void UpdateQuality_AgedBrieQualityUpperLimitExceededAfterSellIn_QualityStaysAtLimit()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = QualityUpperLimit },
-                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 }};
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BRIE, SellIn = 0, Quality = QualityUpperLimit },
+                                                 new Item { Name = GildedRose.BRIE, SellIn = 0, Quality = QualityUpperLimit - 1 }};
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -145,7 +145,7 @@ namespace csharp
         [Test]
         public void UpdateQuality_SulfurasQuality_DoesntChange()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.SUFURAS, SellIn = -1, Quality = 80 } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -157,7 +157,7 @@ namespace csharp
         public void UpdateQuality_SulfurasSellIn_DoesntChange()
         {
             var startingSellIn = -1;
-            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = startingSellIn, Quality = 80 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.SUFURAS, SellIn = startingSellIn, Quality = 80 } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -171,7 +171,7 @@ namespace csharp
         public void UpdateQuality_BackstagePassSellIn_Lowers()
         {
             var startingSellIn = 5;
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = startingSellIn, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = startingSellIn, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -183,8 +183,8 @@ namespace csharp
         public void UpdateQuality_BackstagePassQualityMoreThanTenDaysBeforeConcert_IncreasesByOne()
         {
             var startingQuality = 10;
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 18, Quality = startingQuality },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 18, Quality = startingQuality },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 11, Quality = startingQuality } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -197,8 +197,8 @@ namespace csharp
         public void UpdateQuality_BackstagePassQualityTenDaysOrLessBeforeConcert_IncreasesByTwo()
         {
             var startingQuality = 10;
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = startingQuality },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 10, Quality = startingQuality },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 6, Quality = startingQuality } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -211,8 +211,8 @@ namespace csharp
         public void UpdateQuality_BackstagePassQualityFiveDaysOrLessBeforeConcert_IncreasesByThree()
         {
             var startingQuality = 10;
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = startingQuality },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 5, Quality = startingQuality },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 1, Quality = startingQuality } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -224,7 +224,7 @@ namespace csharp
         [Test]
         public void UpdateQuality_BackstagePassQualityAfterConcert_GoesToZero()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 15 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 0, Quality = 15 } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -235,7 +235,7 @@ namespace csharp
         [Test]
         public void UpdateQuality_BackstagePassQualityUpperLimitExceededWhenMoreThanTenDaysBeforeConcert_QualityStaysAtLimit()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = QualityUpperLimit } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 11, Quality = QualityUpperLimit } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -246,8 +246,8 @@ namespace csharp
         [Test]
         public void UpdateQuality_BackstagePassQualityUpperLimitExceededWhenTenOrLessDaysBeforeConcert_QualityStaysAtLimit()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 50 } };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 10, Quality = QualityUpperLimit - 1 },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 6, Quality = QualityUpperLimit } };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -259,9 +259,9 @@ namespace csharp
         [Test]
         public void UpdateQuality_BackstagePassQualityUpperLimitExceededWhenFiveOrLessDaysBeforeConcert_QualityStaysAtLimit()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48 },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 3, Quality = 49 },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 50 }, };
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.BACKSTAGE, SellIn = 5, Quality = QualityUpperLimit - 2 },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 3, Quality = QualityUpperLimit - 1 },
+                                                 new Item { Name = GildedRose.BACKSTAGE, SellIn = 1, Quality = QualityUpperLimit }, };
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
@@ -272,5 +272,67 @@ namespace csharp
         }
 
         // Conjured Items
+
+        [Test]
+        public void UpdateQuality_ConjuredItemSellInDays_Lowers()
+        {
+            var startingSellIn = 1;
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.CONJURED, SellIn = startingSellIn, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(startingSellIn, Items[0].SellIn + 1);
+        }
+
+        [Test]
+        public void UpdateQuality_ConjuredItemQualityBeforeSellIn_LowersByTwo()
+        {
+            var startingQuality = 5;
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.CONJURED, SellIn = 5, Quality = startingQuality } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(startingQuality, Items[0].Quality + 2);
+        }
+
+        [Test]
+        public void UpdateQuality_ConjuredItemQualityAfterSellIn_LowersByFour()
+        {
+            var startingQuality = 5;
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.CONJURED, SellIn = 0, Quality = startingQuality } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(startingQuality, Items[0].Quality + 4);
+        }
+
+        [Test]
+        public void UpdateQuality_ConjuredItemQualityLowerLimitExceededBeforeSellIn_QualityStaysAtLimit()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.CONJURED, SellIn = 5, Quality = QualityLowerLimit },
+                                                 new Item { Name = GildedRose.CONJURED, SellIn = 5, Quality = QualityLowerLimit + 1 }};
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(QualityLowerLimit, Items[0].Quality);
+            Assert.AreEqual(QualityLowerLimit, Items[1].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_ConjureditemQualityLowerLimitExceededAfterSellIn_QualityStaysAtLimit()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = GildedRose.CONJURED, SellIn = 0, Quality = QualityLowerLimit },
+                                                 new Item { Name = GildedRose.CONJURED, SellIn = 0, Quality = QualityLowerLimit + 3 }};
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(QualityLowerLimit, Items[0].Quality);
+            Assert.AreEqual(QualityLowerLimit, Items[1].Quality);
+        }
     }
 }
